a26938e6ddd39801d0b333833f33d991aecb9c84
Brandon M Wilk <wilkb777@gmail.com>
2023-10-31 14:01:23 -0500

    updating configs, first pass OC return parsing

--- src/parse.py
+++ src/parse.py
@@ -2,11 +2,14 @@ from pathlib import Path
 import argparse
 import os
 import json
+import yaml
+import pandas as pd
 
 
 class OCApiParser:
-    def __init__(self, parsing_config) -> None:
+    def __init__(self, parsing_config, column_config) -> None:
         self.parsing_config = parsing_config
+        self.column_config = column_config
 
     def _parse_all_mappings(self, oc_response_dict):
         if (not oc_response_dict["crx"]["all_mappings"]) or oc_response_dict["crx"][
@@ -20,6 +23,11 @@ class OCApiParser:
             for var_trx in var_trxs:
                 mappings.append(
                     {
+                        "chrom": oc_response_dict["crx"]["chrom"],
+                        "pos": oc_response_dict["crx"]["pos"],
+                        "ref_base": oc_response_dict["crx"]["ref_base"],
+                        "alt_base": oc_response_dict["crx"]["alt_base"],
+                        "coding": oc_response_dict["crx"]["coding"],
                         "gene": gene,
                         "transcript": var_trx[3],
                         "consequence": var_trx[2],
@@ -30,49 +38,125 @@ class OCApiParser:
 
         return mappings
 
-    def _parse_list_of_dicts(self, oc_response_dict, column_config):
-        col_keys = column_config["col_id"].split(".")
-        dict_of_dicts = dict()
-        if not oc_response_dict[col_keys[0]]:
-            return dict_of_dicts
-
-        for sublist in oc_response_dict[col_keys[0]][col_keys[1]]:
-            sublist_dict = dict()
-            trx_id = sublist[column_config["trx_mapping_col_index"]].split(".")[0]
-            dict_of_dicts[trx_id] = sublist_dict
-            for index, value in enumerate(sublist):
-                # look up column name by index value, assign column name as key in return dict
-                if str(index) not in column_config["dict_index"]:
-                    continue
-
-                sublist_dict[column_config["dict_index"][str(index)]] = value
-
-        return dict_of_dicts
-
-    def _parse_multicolumn_list_of_dicts(
-        self, index_column, multi_column_config, data_cols_dict
+    def _parse_list_of_dicts(
+        self,
+        data_src,
+        data_list_field_name,
+        trx_mapping_col_index,
+        field_indexs,
+        oc_response_dict,
     ):
-        # data_cols_n_configs => list of tuples where tuple[0] is column value, tuple[1] is column config
-        index_mapping = dict()
         dict_of_dicts = dict()
-
-        if index_column == "":
+        if not oc_response_dict[data_src]:
             return dict_of_dicts
 
-        for index, index_value in enumerate(
-            index_column.split(multi_column_config["separator"])
-        ):
-            index_mapping[index] = index_value.split(".")[0]
-            dict_of_dicts[index_mapping[index]] = dict()
-
-        for column, value in data_cols_dict.items():
-            sublist = value.split(multi_column_config["separator"])
-            for index, data_value in enumerate(sublist):
-                dict_of_dicts[index_mapping[index]][column] = data_value
+        for sublist in oc_response_dict[data_src][data_list_field_name]:
+            trx_id = sublist[trx_mapping_col_index].split(".")[0]
+            dict_of_dicts[trx_id] = {
+                data_field_name: sublist[int(index)]
+                for index, data_field_name in field_indexs.items()
+            }
 
         return dict_of_dicts
 
-    def parse_oc_api_json(self, annot_json_string: str) -> list:
+    def _parse_indexed_list(
+        self, data_src, index_field_name, data_fields, value_sep, oc_response_dict
+    ):
+        if not oc_response_dict[data_src]:
+            return dict()
+
+        trx_index_keys = [
+            key.split(".")[0]
+            for key in oc_response_dict[data_src][index_field_name].split(value_sep)
+        ]
+        dict_of_dicts = {trx: dict() for trx in trx_index_keys}
+
+        for data_field in data_fields:
+            data_field_name = data_field.split(".")[1]
+            for index, data_value in enumerate(
+                oc_response_dict[data_src][data_field_name].split(value_sep)
+            ):
+                dict_of_dicts[trx_index_keys[index]][data_field_name] = data_value
+
+        return dict_of_dicts
+
+    def _parse_normal_field(self, data_src, data_field_name, oc_response_dict):
+        if not oc_response_dict[data_src]:
+            return None
+
+        return oc_response_dict[data_src][data_field_name]
+
+    def parse_oc_api_json(self, annotations_dict: str) -> list:
+        listy_fields_mapping = []
+
+        # parse out list type fields
+        for list_field_config in filter(
+            lambda feild_config: "list" in feild_config["parse_type"],
+            self.parsing_config,
+        ):
+            data_source = list_field_config["col_id"].split(".")[0]
+            trx_index_field = list_field_config["parse_type"]["list"][
+                "trx_index_col"
+            ].split(".")[1]
+            field_separator = list_field_config["parse_type"]["list"]["separator"]
+            data_fields = list_field_config["parse_type"]["list"]["column_list"]
+            listy_fields_mapping.append(
+                self._parse_indexed_list(
+                    data_src=data_source,
+                    index_field_name=trx_index_field,
+                    data_fields=data_fields,
+                    value_sep=field_separator,
+                    oc_response_dict=annotations_dict,
+                )
+            )
+
+        # parse out list of dicts fields
+        for list_field_config in filter(
+            lambda feild_config: "list-o-dicts" in feild_config["parse_type"],
+            self.parsing_config,
+        ):
+            data_source_cols = list_field_config["col_id"].split(".")
+            trx_col_index = list_field_config["parse_type"]["list-o-dicts"][
+                "trx_mapping_col_index"
+            ]
+            data_fields = list_field_config["parse_type"]["list-o-dicts"]["dict_index"]
+            listy_fields_mapping.append(
+                self._parse_list_of_dicts(
+                    data_src=data_source_cols[0],
+                    data_list_field_name=data_source_cols[1],
+                    trx_mapping_col_index=trx_col_index,
+                    field_indexs=data_fields,
+                    oc_response_dict=annotations_dict,
+                )
+            )
+
+        # parse regular fields
+        reg_fields = dict()
+        for reg_field in filter(
+            lambda feild_config: "none" in feild_config["parse_type"],
+            self.parsing_config,
+        ):
+            cols = reg_field["col_id"].split(".")
+            reg_fields[reg_field["col_id"]] = self._parse_normal_field(
+                data_src=cols[0],
+                data_field_name=cols[1],
+                oc_response_dict=annotations_dict,
+            )
+
+        # iterate all mappings listing which provides all variant + transcript combo info to build return list
+        ret_list = []
+        for var_trx in self._parse_all_mappings(annotations_dict):
+            var_trx.update(reg_fields)
+            trx_key = var_trx["transcript"].split(".")[0]
+            for listy_dict in listy_fields_mapping:
+                if trx_key in listy_dict:
+                    var_trx.update(listy_dict[trx_key])
+
+            ret_list.append(var_trx)
+
+        return ret_list
+
+    def query_variant(self, chrom: str, pos: int, ref: str, alt: str) -> pd.DataFrame:
         pass
 
 
