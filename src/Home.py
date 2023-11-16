4559a6a4ff77dea66608524ff94aa10980ec5c4b
Tarun Mamidi <tmamidi@uab.edu>
2023-11-16 16:47:27 -0600

    working website

--- src/Home.py
+++ src/Home.py
@@ -7,37 +7,6 @@ st.set_page_config(
     layout="wide",  # initial_sidebar_state="expanded",
 )
 
-
-def sidebar_links():
-    database_link_dict = {
-        "GitHub Page": "https://github.com/uab-cgds-worthey/DITTO4NF",
-        "RCSB Protein Data Bank": "https://www.rcsb.org",
-    }
-
-    st.sidebar.markdown("## Database-Related Links")
-    for link_text, link_url in database_link_dict.items():
-        st.sidebar.markdown(f"[{link_text}]({link_url})")
-
-    software_link_dict = {
-        "3Dmol": "https://3dmol.csb.pitt.edu",
-        "Pandas": "https://pandas.pydata.org",
-        "SHAP": "https://shap.readthedocs.io/en/latest/index.html",
-        "Matplotlib": "https://matplotlib.org",
-        "Streamlit": "https://streamlit.io",
-    }
-
-    st.sidebar.markdown("## Software-Related Links")
-    link_1_col, link_2_col, link_3_col = st.sidebar.columns(3)
-    i = 0
-    link_col_dict = {0: link_1_col, 1: link_2_col, 2: link_3_col}
-    for link_text, link_url in software_link_dict.items():
-        st_col = link_col_dict[i]
-        i += 1
-        if i == len(link_col_dict.keys()):
-            i = 0
-        st_col.markdown(f"[{link_text}]({link_url})")
-
-
 st.markdown("# DITTO")
 st.markdown("\n\n")
 st.markdown(
@@ -51,8 +20,6 @@ st.markdown(
 st.markdown("\n\n")
 st.markdown("\n\n")
 st.markdown("**[Center for Computational Genomics and Data Science](https://sites.uab.edu/cgds/)**")
-
-sidebar_links()
 
 st.markdown("---")
 
--- src/pages/2_Prediction.py
+++ src/pages/2_Prediction.py
@@ -1,22 +1,19 @@
 import streamlit as st
 
+import matplotlib.pyplot as plt
 st.set_option(
     "deprecation.showPyplotGlobalUse", False
 )  # To suppress MatplotlibDeprecationWarning to display SHAP plot
 import pandas as pd
-import plotly.express as px
-import numpy as np
 import yaml
 import pickle
-
-import requests
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
-
-# import matplotlib.pyplot as plt
+import json
+from parse import OCApiParser
+from predict import parse_and_predict
 import shap
 from tensorflow import keras
-
+import numpy as np
+from pathlib import Path
 
 # Config the whole app
 st.set_page_config(
@@ -34,6 +31,15 @@ def get_col_configs(config_f):
 
     # print(config_dict)
     return config_dict
+
+# Function to open and load data config file for parsing opencravat output
+@st.cache_data
+def get_parser(data_config):
+    with data_config.open("rt") as dc_fp:
+        data_config_dict = json.load(dc_fp)
+
+    parser = OCApiParser(data_config_dict)
+    return parser
 
 
 # Class to define colors for Clinvar classification
@@ -78,121 +84,22 @@ def load_model():
     explainer = shap.KernelExplainer(clf.predict, background)
     return explainer, clf
 
-
-def query_external_api(api_url):
-    headers = {"Content-Type": "application/json;charset=UTF-8"}
-    retry_strategy = Retry(total=3, backoff_factor=1, status_forcelist=[429, 500, 502, 503, 504])
-    adapter = HTTPAdapter(max_retries=retry_strategy)
-    http = requests.Session()
-    http.mount("https://", adapter)
-    response = http.get(api_url, headers=headers)
-
-    # raise exception if 4XX or 5XX status code returned from server after 3 retries
-    resp_dict = None
-    try:
-        response.raise_for_status()
-        resp_dict = response.json()
-    except requests.exceptions.RequestException as expt:
-        print(
-            f"Failed to query {api_url}, code: {response.status_code}, reason: {response.reason},"
-            f" text: {response.text}, error: {str(expt)}"
-        )
-    finally:
-        http.close()  # this will release resources so that the GC can clean up the request and response objects
-
-    return resp_dict
-
-
-@st.cache_data(
-    ttl=3600
-)  # expire cached results of the Uniprot query after 1 hour for the requested gene
-def get_domain(gene_name):
-    uniprot_url = f"https://rest.uniprot.org/uniprotkb/search?query=gene_exact:{gene_name}+AND+organism_id:9606&format=json&fields=ft_domain,cc_domain,protein_name"
-    info_dict = query_external_api(uniprot_url)
-
-    # set defaults for uniprot return information
-    fullname = ""
-    domain = []
-
-    if len(info_dict["results"]) > 0:
-        first_rec = info_dict["results"][0]
-        if "recommendedName" in first_rec.get("proteinDescription"):
-            fullname = (
-                first_rec.get("proteinDescription")
-                .get("recommendedName")
-                .get("fullName")
-                .get("value")
-            )
-        elif "submissionNames" in first_rec.get("proteinDescription"):
-            fullname = (
-                first_rec.get("proteinDescription")
-                .get("submissionNames")[0]
-                .get("fullName")
-                .get("value")
-            )
-
-        for feature in info_dict["results"][0]["features"]:
-            if feature["type"] == "Domain":
-                domain.append(
-                    {
-                        "type": "Domain",
-                        "description": feature["description"],
-                        "start": int(feature["location"]["start"]["value"]),
-                        "end": int(feature["location"]["end"]["value"]),
-                    }
-                )
-
-    if len(domain) == 0:
-        domain.append({"type": "Domain", "description": "", "start": 0, "end": 0})
-
-    del info_dict
-    return fullname, pd.DataFrame(domain)
-
-
-@st.cache_data(
-    ttl=3600
-)  # expire cached results of the Uniprot query after 1 hour for the requested gene
-def oc_api(chrom="1", pos=2406483, ref="C", alt="G"):
-    uniprot_url = f"https://run.opencravat.org/submit/annotate?chrom=chr{chrom}&pos={pos}&ref_base={ref}&alt_base={alt}&annotators=aloft,cadd,cadd_exome,cancer_genome_interpreter,ccre_screen,chasmplus,civic,clingen,clinpred,clinvar,cosmic,cosmic_gene,cscape,dann,dann_coding,dbscsnv,dbsnp,dgi,ensembl_regulatory_build,ess_gene,exac_gene,fathmm,fathmm_xf_coding,funseq2,genehancer,gerp,ghis,gnomad,gnomad3,gnomad_gene,gtex,gwas_catalog,linsight,loftool,lrt,mavedb,metalr,metasvm,mutation_assessor,mutationtaster,mutpred1,mutpred_indel,ncbigene,ndex,ndex_chd,ndex_signor,omim,pangalodb,phastcons,phdsnpg,phi,phylop,polyphen2,prec,provean,repeat,revel,rvis,segway,sift,siphy,spliceai,uniprot,vest,cgc,cgd,varity_r"
-    info_dict = query_external_api(uniprot_url)
-    return info_dict
-
-
-def get_annots(info_dict, config_dict):
-    current_data = {}
-
-    for key in config_dict:
-        if key in info_dict:
-            current_data[key] = info_dict[key]
-        else:
-            # Handle the case where the key is not present in the dictionary
-            current_data[key] = None
-    return current_data
-
-
 def main():
+    repo_root = Path(__file__).parent.parent.parent
+
     st.header(f"DITTO deleterious prediction with explanations")
 
-    # Load the config file as dictionary
-    config_f = "./configs/col_config.yaml"
+    # Load the col config file as dictionary
+    config_f = repo_root / "configs" / "col_config.yaml"
     config_dict = get_col_configs(config_f)
+
+    # Load the data config file as dictionary
+    data_config = repo_root / "configs" / "opencravat_test_config.json"
+    parser = get_parser(data_config)
 
     # Load the model and data
     # df2, var, feature_names = load_lovd(config_dict)
     explainer, clf = load_model()
-
-    # # Filter data depending on transcript selected
-    # transcript = st.sidebar.selectbox("Choose transcript:", options=df2["transcript"].unique())
-    # lovd = df2[df2["transcript"] == transcript].reset_index(drop=True)
-    # overall = var[var["transcript"] == transcript].reset_index(drop=True)
-
-    # # Download DITTO predictions
-    # st.sidebar.download_button(
-    #     label=f"Download DITTO predictions",
-    #     data=convert_df(overall),
-    #     file_name=f"DITTO_LOVD_predictions.csv",
-    #     mime="text/csv",
-    # )
 
     col1, col2, col3, col4 = st.columns(4)
     chrom = col1.selectbox("Chromosome:", options=list(range(1, 22)) + ["X", "Y", "MT"])
@@ -200,65 +107,63 @@ def main():
     ref = col3.text_input("Reference Nucleotide:", "C")
     alt = col4.text_input("Alternate Nucleotide:", "G")
 
-    # Filter variant from data and display variant information for SHAP plot
-    st.subheader("**DITTO Explanations using SHAP**")
-    st.markdown(
-        "Please choose table view option in sidebar to display necessary"
-        " variant information that can be used to generate SHAP plot."
-    )
-    # if chrom & pos & ref & alt:
-    info_dict = oc_api(chrom, int(pos), ref, alt)
-    annot_col1, annot_col2, annot_col3, annot_col4 = st.columns(4)
-    annot_col1.subheader("Allele Frequencies")
-    AF_data = get_annots(info_dict, config_dict["display_cols"]["AF"])
-    annot_col1.write(AF_data)
+    if str(chrom) & str(pos) & ref & alt:
+        overall = parser.query_variant(
+                chrom=str(chrom), pos=int(pos), ref=ref, alt=alt
+            )
 
-    annot_col2.subheader("Conservation scores")
-    cons_data = get_annots(info_dict, config_dict["display_cols"]["cons"])
-    annot_col2.write(cons_data)
+        st.subheader("**OpenCravat annotations**")
+        st.dataframe(overall)
 
-    annot_col3.subheader("Damage predictors")
-    pred_data = get_annots(info_dict, config_dict["display_cols"]["pred"])
-    annot_col3.write(pred_data)
+        st.write("\n\n")
+        transcript = st.selectbox("Choose a transcript:", options=list(overall['transcript'].unique()))
+        transcript_data = overall[overall['transcript'] == transcript].reset_index(drop=True)
+        df2, y_score = parse_and_predict(transcript_data, config_dict, clf)
 
-    annot_col4.subheader("Function")
-    clinical_data = get_annots(info_dict, config_dict["display_cols"]["clinical"])
-    annot_col4.write(clinical_data)
+        # Download DITTO predictions
+        st.download_button(
+            label=f"Download annotations",
+            data=convert_df(overall),
+            file_name=f"OC_annotations.csv",
+            mime="text/csv",
+        )
 
-    # st.write(info_dict)
+        st.subheader("**DITTO prediction and explanations using SHAP**")
 
-    # # initialise data of lists to print on the webpage.
-    # var_scores = {
-    #     "Source": [
-    #         "DITTO score",
-    #         "Clinvar Classification",
-    #         "Clingen Classification",
-    #         "gnomAD AF",
-    #     ],
-    #     "Values": [
-    #         # str(round(1 - (ditto["DITTO"].values[0]), 2)),
-    #         str(ditto["DITTO"].values[0]),
-    #         ditto["clinvar.sig"].values[0],
-    #         ditto["clingen.classification"].values[0],
-    #         annots["gnomad3.af"].values[0],
-    #     ],
-    # }
-    # # Create DataFrame
-    # col1.dataframe(pd.DataFrame(var_scores))
+        pred_col1, pred_col2 = st.columns(2)
 
-    # # SHAP explanation
-    # shap_value = explainer.shap_values(annots.values)[0]
+        # initialise data of lists to print on the webpage.
+        var_scores = {
+            "Source": [
+                "DITTO score",
+                "Clinvar Classification",
+                "Clingen Classification",
+                "gnomAD AF",
+            ],
+            "Values": [
+                # str(round(1 - (ditto["DITTO"].values[0]), 2)),
+                str(y_score[0][0]),
+                transcript_data["clinvar.sig"].values[0],
+                transcript_data["clingen.classification"].values[0],
+                transcript_data["gnomad3.af"].values[0],
+            ],
+        }
+        # Create DataFrame
+        pred_col1.dataframe(pd.DataFrame(var_scores))
 
-    # # SHAP explanation plot for a variant
-    # col2.pyplot(
-    #     shap.plots._waterfall.waterfall_legacy(
-    #         1 - explainer.expected_value[0],  # DITTO prediction for deleterious is (1 - y_pred)
-    #         np.negative(shap_value[0]),  # SHAP value for deleterious is negative
-    #         annots.values[0],
-    #         feature_names,
-    #         max_display=20,
-    #     )
-    # )
+        # SHAP explanation
+        shap_value = explainer.shap_values(df2.values)[0]
+
+        # SHAP explanation plot for a variant
+        pred_col2.pyplot(
+            shap.plots._waterfall.waterfall_legacy(
+                1 - explainer.expected_value[0],  # DITTO prediction for deleterious is (1 - y_pred)
+                np.negative(shap_value[0]),  # SHAP value for deleterious is negative
+                df2.values[0],
+                df2.columns,
+                max_display=20,
+            )
+        )
 
 
 if __name__ == "__main__":
